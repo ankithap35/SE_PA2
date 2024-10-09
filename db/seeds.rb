ed618fbb77cde9d822b5646cc6ce462c964add58
Ankitha Prasad <ankithap@tamu.edu>
2024-10-09 15:35:18 -0500

    Fix syntax error in seeds.rb and add new movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,18 @@
 
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  { title: 'My Neighbor Totoro', rating: 'G',
-    release_date: '16-Apr-1988' },
-  { title: 'Green Book', rating: 'PG-13',
-    release_date: '16-Nov-2018' },
-  { title: 'Parasite', rating: 'R',
-    release_date: '30-May-2019' },
-  { title: 'Nomadland', rating: 'R',
-    release_date: '19-Feb-2021' },
-  { title: 'CODA', rating: 'PG-13',
-    release_date: '13-Aug-2021' }
+  { title: 'My Neighbor Totoro', rating: 'G', release_date: '16-Apr-1988' },
+  { title: 'Green Book', rating: 'PG-13', release_date: '16-Nov-2018' },
+  { title: 'Parasite', rating: 'R', release_date: '30-May-2019' },
+  { title: 'Nomadland', rating: 'R', release_date: '19-Feb-2021' },
+  { title: 'CODA', rating: 'PG-13', release_date: '13-Aug-2021' },
+  { title: 'The Shawshank Redemption', rating: 'R', release_date: '14-Oct-1994' }, # Added comma here
+  { title: 'The Dark Knight', rating: 'PG-13', release_date: '18-Jul-2008' }, # Added comma here
+  { title: 'The Godfather', rating: 'R', release_date: '24-Mar-1972' } # Added comma here
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  if !(Movie.find_by(title: movie[:title]))
+    Movie.create!(movie)
+  end
 end
